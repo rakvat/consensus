73c2a422a15634691aa81c45c8586db4d95c9486
rakvat <rakvat@gmx.net>
2014-07-28 23:41:07 +0200

    first spike using neo4j

--- src/controller/consensus_greeting_controller.erl
+++ src/controller/consensus_greeting_controller.erl
@@ -3,3 +3,15 @@
 
 hello('GET', []) ->
   {output, "Hello, world! Let's start!"}.
+
+hi_neo('GET', []) ->
+  Neo = neo4j:connect([{base_uri, <<"http://localhost:7474/db/data/">>}]),
+  Result = neo4j:get_node(Neo, 1),
+  Data = lkup(<<"data">>, Result),
+  A = lkup(<<"a">>, Data),
+  {output, A}.
+
+lkup(What, {List}) ->
+      lkup(What, List);
+lkup(What, List) ->
+      proplists:get_value(What, List).
